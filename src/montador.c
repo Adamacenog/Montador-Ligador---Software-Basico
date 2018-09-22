910866c62157b5edfb87b0c4879473683779e764
Andre Garrido <andre.garridod@gmail.com>
2018-09-22 16:29:41 -0300

    Inicio da codificação do montador

--- src/montador.c
+++ src/montador.c
@@ -11,8 +11,25 @@ Jônatas Senna - mat.
     #include <string.h>
 #endif
 
-int main()
+#ifndef _Montador_library
+  #define _Montador_library
+    #include "montador.h"
+#endif
+
+int main(int argc, char *argv[])
 {
-  printf("Montador\n");
+  if(argc == 2)
+  {
+    printf("O arquivo '.asm' fornecido foi: %s\n", argv[1]);
+  }
+  else if(argc > 2)
+  {
+    printf("ERRO: Muitos arquivos passados, é possível montar apenas um arquivo '.asm' por vez.\n");
+  }
+  else
+  {
+    printf("ERRO: Ao menos um arquivo '.asm' é esperado.\n");
+  }
+
   return 0;
 }
