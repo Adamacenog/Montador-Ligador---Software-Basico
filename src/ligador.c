23f8845e2d6bd9e4111e844d5dfcd842aa46e7b1
Andre Garrido <andre.garridod@gmail.com>
2018-10-20 16:09:00 -0300

    Revert "Correções ~ Ainda falta ajustar o problema do '\n' para modulos"
    This reverts commit 09670108875f8eaed837fc1c590a08e3b9278d9e.

--- src/ligador.c
+++ src/ligador.c
@@ -52,7 +52,7 @@ int main(int argc, char** argv)
       fscanf(object, "%c",&auxChar[0]);
   if(auxChar[0]<58 && auxChar[0]>47 )
   {
-    while(!feof(object) && auxChar[0]!='\n')
+    while(!feof(object) || auxChar[0]=='\n')
     {
       fprintf(output, "%c", auxChar[0]);
       fscanf(object, "%c",&auxChar[0]);
@@ -139,22 +139,15 @@ int main(int argc, char** argv)
           while(!feof(object))
           {
             fscanf(object, "%s",auxChar);
-            if(strcmp(auxChar, "\n") != 0)
+            number = strtol(auxChar,&dump, 10);
+            if(code==NULL)
             {
-              number = strtol(auxChar,&dump, 10);
-              if(code==NULL)
-              {
-                code = addCodeObj(code, number, offsetAux[i-1]+offset);
-              }else
-              {
-                addCodeObj(code, number, offsetAux[i-1]+offset);
-              }
-              offset++;
-            }
-            else
+              code = addCodeObj(code, number, offsetAux[i-1]+offset);
+            }else
             {
-              break;
+              addCodeObj(code, number, offsetAux[i-1]+offset);
             }
+            offset++;
           }
           offsetAux[i] = offset+offsetAux[i-1];
           break;
